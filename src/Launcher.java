0870cf57f87025a0c282459e3de0225570bb1349
Nico <n.michelis@gmail.com>
2019-11-17 15:33:45 +0100

    Refactor messaggi

--- src/Launcher.java
+++ src/Launcher.java
@@ -17,57 +17,52 @@ public class Launcher {
 	private static HashMap<String, Node> listaNodi = new HashMap<String, Node>();
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("INIZIO");
+		System.out.println("[Main] Start");
 		Node t = null;
 		try {
 			ip = InetAddress.getLocalHost().getHostAddress();
 		} catch (UnknownHostException e) {
+			System.err.println("[Main] Could not get ip address");
 			e.printStackTrace();
+			System.exit(-1);
 		}
-
+		
+		// Creo nuovi nodi
 		for (int i = 0; i < nNodes; i++) {
 			t = new Node(i, ip);
 			listaNodi.put(ip + ":" + String.valueOf(t.getPort()), t);
 		}
 
+		// Inoltro a tutti i nodi l'indirizzo degli altri
 		Iterator<Node> it = listaNodi.values().iterator();
 		while (it.hasNext()) {
 			Node p = it.next();
-			// Vengono mandati tutti gli indirizzi, anche di se stesso; verrà
-			// poi scartato
 			listaNodi.forEach((k, v) -> p.addAddress(v.getFullAddress()));
 		}
 
-		// Run nodes
+		// Stampo elenco nodi in memoria
+		printNodeList();
+		
+		// Faccio eseguire i nodi
 		for (Map.Entry<String, Node> entry : listaNodi.entrySet()) {
 			Thread thread = new Thread(entry.getValue());
 			thread.start();
 		}
 
-		// Main cycle
+		// Main
 		boolean exit = false;
 		while (!exit) {
-			System.out.println("Elenco nodi in memoria (numero porta):");
-			listaNodi.forEach((k, v) -> System.out.println(k.split(":")[1]));
-
 			// Gestione dati acquisiti dalla scrittura su console
 			Scanner in = new Scanner(System.in);
 			String s = in.nextLine();
-			
-			// Se viene inserito qualcosa da tastiera
-			commandTranslation(s);
-			
-			int leader = 0;
-			for (Map.Entry<String, Node> entry : listaNodi.entrySet()) {
-				//String key = entry.getKey();
-				Node node = entry.getValue();
-				// System.out.println(" "+key + " " + node.getRole());
-				if (node.getRole().equals(Role.LEADER))
-					leader++;
-			}
-			if (leader > 1)
-				System.exit(-1);
+			exit = commandTranslation(s);
+			in.close();
 		}
+	}
+
+	private static void printNodeList() {
+		System.out.println("[Main] List of nodes (port number):");
+		listaNodi.forEach((k, v) -> System.out.println(" - " + k.split(":")[1]));
 	}
 
 	/**
@@ -92,8 +87,9 @@ public class Launcher {
 	 * 'stop heartbeats' <- per far smettere al leader di mandare heartbeats, ciò porterò
 	 * 		al cambio di leader
 	 * @param s Stringa inserita dall'utente
+	 * @return 
 	 */
-	public static void commandTranslation(String s){
+	public static boolean commandTranslation(String s){
 		String[] split = s.split(" ");
 		switch (split[0]) {
 		case "set":
@@ -101,17 +97,17 @@ public class Launcher {
 			case "electionTimeout":
 				if (split.length >= 4 && isNumeric(split[3]) && listaNodi.containsKey(ip 
 						+ ":" + split[2])) {
-					System.out.println("setting election timeout of " + split[2] + " to "
+					System.out.println("[Main] Setting election timeout of " + split[2] + " to "
 						+ split[3]);
 					listaNodi.get(ip + ":" + split[2]).setElectionTimeout(Integer.valueOf
 							(split[3]));
 				} else {
-					System.err.println("Error: invalid input in election timeout");
+					System.err.println("[Main] Error: invalid input in election timeout");
 				}
 				break;
 			
 			default:
-				System.err.println("Error: invalid input set");
+				System.err.println("[Main] Error: invalid input set");
 				break;
 			}
 			break;
@@ -121,8 +117,10 @@ public class Launcher {
 				Iterator<Map.Entry<String, Node>> ite = listaNodi.entrySet().iterator();
 				while (ite.hasNext()) {
 				    Node n = ite.next().getValue();
-				    if(n.getRole().equals(Role.LEADER))
+				    if(n.getRole().equals(Role.LEADER)) {
 				    	n.stopHeartbeats();
+				    	System.out.println("[Main] Stopped heartbeat on "+n.getFullAddress());
+				    }
 				}
 				break;
 			default:
@@ -132,21 +130,29 @@ public class Launcher {
 		case "check":
 			switch (split[1]) {
 			case "state":
-				System.out.println("State of " + split[2] + " is "+
+				System.out.println("[Main] State of " + split[2] + " is "+
 						listaNodi.get(ip + ":" + split[2]).getStateMachine().toString());
 				break;
 			case "log":
-				System.out.println("Log of " + split[2] + " is "+
+				System.out.println("[Main] Log of " + split[2] + " is "+
 						listaNodi.get(ip + ":" + split[2]).getLog().toString());
 				break;
+			case "term":
+				System.out.println("[Main] Term of " + split[2] + " is "+
+						listaNodi.get(ip + ":" + split[2]).getCurrentTerm());
+				break;
 			default:
-				System.err.println("Error: invalid input set");
+				System.err.println("[Main] Error: invalid input set");
 				break;
 			}
 			break;
+		case "exit":
+			System.out.println("[Main] Shutting down");
+			return true;
 		default:
-			System.err.println("Error: invalid input");
+			System.err.println("[Main] Error: invalid input");
 			break;
 		}
+		return false;
 	}
 }
